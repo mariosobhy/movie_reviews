27f2168e56f82ef91fd9e1f5b9ea7df9b5a9449f
mariosobhy <mariosobhy0123@outlook.com>
2024-03-06 23:28:25 +0200

    Support search case-insensitive

--- app/repositories/movie_repository.rb
+++ app/repositories/movie_repository.rb
@@ -1,10 +1,9 @@
 class MovieRepository
-    def self.search_by_actor(actor)
-      movies = Movie.left_joins(:reviews)
-                    .group(:id)
-                    .order('AVG(reviews.stars) DESC NULLS LAST')
-  
-      actor.present? ? movies.where("movies.actor LIKE ?", "%#{actor}%") : movies
-    end
+  def self.search_by_actor(actor)
+    movies = Movie.left_joins(:reviews)
+                  .group(:id)
+                  .order('AVG(reviews.stars) DESC NULLS LAST')
+
+    actor.present? ? movies.where("LOWER(movies.actor) LIKE ?", "%#{actor.downcase}%") : movies
+  end
 end
-  
